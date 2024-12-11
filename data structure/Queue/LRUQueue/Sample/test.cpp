3e5446075b5f06884ba69c89c09c93d8e8a8f596
solifree <solifree@qq.com>
2024-12-11 23:11:24 +0800

    Update LRUQueue v1.1.0

--- data structure/Queue/LRUQueue/Sample/test.cpp
+++ data structure/Queue/LRUQueue/Sample/test.cpp
@@ -1,6 +1,7 @@
 ﻿#include "LRUQueue.hpp"
 
 #include <cstdlib>
+#include <cstddef>
 #include <functional>
 #include <iostream>
 
@@ -8,14 +9,14 @@ struct Key
 {
 	int _integer;
 
-	Key(decltype(_integer) _integer) noexcept : \
-		_integer(_integer) {}
-
 	friend bool operator==(const Key& _left, \
 		const Key& _right) noexcept
 	{
 		return _left._integer == _right._integer;
 	}
+
+	Key(decltype(_integer) _integer) noexcept : \
+		_integer(_integer) {}
 };
 
 namespace std
@@ -33,27 +34,29 @@ namespace std
 
 int main()
 {
-	using QueueType = LRUQueue<Key, int>;
-	QueueType queue(9);
+	using CacheType = LRUQueue<Key, int>;
+	using QueueType = CacheType::QueueType;
+
+	CacheType cache(9);
 	auto index = 0;
 	for (; index < 10; ++index)
-		queue.push(index, index);
+		cache.push(index, index);
 
-	if (auto result = queue.find(1))
+	if (auto result = cache.find(1))
 		std::cout << result.value() << std::endl;
 
-	std::cout << queue.size() << std::endl;
-	queue.pop(--index);
-	std::cout << queue.size() << std::endl;
+	std::cout << cache.size() << std::endl;
+	cache.pop(--index);
+	std::cout << cache.size() << std::endl;
 
-	if (QueueType::VectorType vector; \
-		queue.pop(vector))
+	if (QueueType queue; cache.pop(queue))
 	{
-		for (const auto& pair : vector)
+		for (const auto& pair : queue)
 			std::cout << pair.second << ' ';
 		std::cout << '\b' << std::endl;;
 	}
 
-	std::cout << std::boolalpha << queue.empty() << std::endl;
+	std::cout << std::boolalpha \
+		<< cache.empty() << std::endl;
 	return EXIT_SUCCESS;
 }
---
+++ data structure/Queue/LRUQueue/Source/LRUQueue.hpp
@@ -0,0 +1,205 @@
+﻿#pragma once
+
+#include <optional>
+#include <utility>
+#include <list>
+#include <unordered_map>
+
+template <typename _KeyType, typename _ValueType>
+class LRUQueue final
+{
+public:
+	using KeyType = _KeyType;
+	using ValueType = _ValueType;
+
+	using PairType = std::pair<KeyType, ValueType>;
+	using QueueType = std::list<PairType>;
+	using SizeType = QueueType::size_type;
+
+private:
+	using Iterator = QueueType::iterator;
+	using TableType = std::unordered_map<KeyType, Iterator>;
+
+private:
+	SizeType _capacity;
+	QueueType _queue; // pair(key, value)
+	TableType _table; // key -> iterator(_queue)
+
+private:
+	void move(Iterator& _iterator);
+
+	void erase();
+
+public:
+	// 若_capacity小于等于零，则无限制，否则其为上限值
+	LRUQueue(decltype(_capacity) _capacity = 0) : \
+		_capacity(_capacity) {}
+
+	auto capacity() const noexcept { return _capacity; }
+	void reserve(decltype(_capacity) _capacity) noexcept
+	{
+		this->_capacity = _capacity;
+	}
+
+	bool empty() const noexcept { return _queue.empty(); }
+	auto size() const noexcept { return _queue.size(); }
+
+	bool exist(const KeyType& _key) const
+	{
+		return _table.contains(_key);
+	}
+
+	bool find(const KeyType& _key, ValueType& _value);
+	std::optional<ValueType> find(const KeyType& _key);
+
+	void push(const KeyType& _key, const ValueType& _value);
+	void push(const KeyType& _key, ValueType&& _value);
+
+	bool pop(const KeyType& _key, ValueType& _value);
+	std::optional<ValueType> pop(const KeyType& _key);
+
+	bool pop(QueueType& _queue);
+
+	void clear() noexcept;
+};
+
+template <typename _KeyType, typename _ValueType>
+void LRUQueue<_KeyType, _ValueType>::move(Iterator& _iterator)
+{
+	decltype(_queue) queue;
+	queue.splice(queue.end(), _queue, _iterator);
+	_queue.splice(_queue.end(), queue);
+}
+
+template <typename _KeyType, typename _ValueType>
+void LRUQueue<_KeyType, _ValueType>::erase()
+{
+	if (_capacity <= 0) return;
+
+	while (size() >= _capacity)
+	{
+		auto iterator = _queue.cbegin();
+		_table.erase(iterator->first);
+		_queue.erase(iterator);
+	}
+}
+
+template <typename _KeyType, typename _ValueType>
+bool LRUQueue<_KeyType, _ValueType>::find(const KeyType& _key, \
+	ValueType& _value)
+{
+	auto iterTable = _table.find(_key);
+	if (iterTable == _table.end()) return false;
+
+	auto& iterQueue = iterTable->second;
+	_value = iterQueue->second;
+
+	move(iterQueue);
+	return true;
+}
+
+template <typename _KeyType, typename _ValueType>
+auto LRUQueue<_KeyType, _ValueType>::find(const KeyType& _key) \
+-> std::optional<ValueType>
+{
+	auto iterTable = _table.find(_key);
+	if (iterTable == _table.end()) return std::nullopt;
+
+	auto& iterQueue = iterTable->second;
+	auto& value = iterQueue->second;
+
+	move(iterQueue);
+	return value;
+}
+
+template <typename _KeyType, typename _ValueType>
+void LRUQueue<_KeyType, _ValueType>::push(const KeyType& _key, \
+	const ValueType& _value)
+{
+	auto iterTable = _table.find(_key);
+	if (iterTable == _table.end())
+	{
+		erase();
+
+		auto iterQueue = _queue.emplace(_queue.end(), \
+			_key, _value);
+		_table.emplace(_key, iterQueue);
+	}
+	else
+	{
+		auto& iterQueue = iterTable->second;
+		iterQueue->second = _value;
+
+		move(iterQueue);
+	}
+}
+
+template <typename _KeyType, typename _ValueType>
+void LRUQueue<_KeyType, _ValueType>::push(const KeyType& _key, \
+	ValueType&& _value)
+{
+	auto iterTable = _table.find(_key);
+	if (iterTable == _table.end())
+	{
+		erase();
+
+		auto iterQueue = _queue.emplace(_queue.end(), \
+			_key, std::forward<ValueType>(_value));
+		_table.emplace(_key, iterQueue);
+	}
+	else
+	{
+		auto& iterQueue = iterTable->second;
+		iterQueue->second = std::forward<ValueType>(_value);
+
+		move(iterQueue);
+	}
+}
+
+template <typename _KeyType, typename _ValueType>
+bool LRUQueue<_KeyType, _ValueType>::pop(const KeyType& _key, \
+	ValueType& _value)
+{
+	auto iterTable = _table.find(_key);
+	if (iterTable == _table.end()) return false;
+
+	auto& iterQueue = iterTable->second;
+	_value = std::move(iterQueue->second);
+
+	_queue.erase(iterQueue);
+	_table.erase(iterTable);
+	return true;
+}
+
+template <typename _KeyType, typename _ValueType>
+auto LRUQueue<_KeyType, _ValueType>::pop(const KeyType& _key) \
+-> std::optional<ValueType>
+{
+	auto iterTable = _table.find(_key);
+	if (iterTable == _table.end()) return std::nullopt;
+
+	auto& iterQueue = iterTable->second;
+	std::optional result = std::move(iterQueue->second);
+
+	_queue.erase(iterQueue);
+	_table.erase(iterTable);
+	return result;
+}
+
+template <typename _KeyType, typename _ValueType>
+bool LRUQueue<_KeyType, _ValueType>::pop(QueueType& _queue)
+{
+	if (empty()) return false;
+
+	_queue.splice(_queue.end(), this->_queue);
+
+	clear();
+	return true;
+}
+
+template <typename _KeyType, typename _ValueType>
+void LRUQueue<_KeyType, _ValueType>::clear() noexcept
+{
+	_table.clear();
+	_queue.clear();
+}
